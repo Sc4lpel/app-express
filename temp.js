0bd44e047603bc3edb2d5a3b744a0a6fd93b7781
Scalpel <sc4p4l@gmail.com>
2018-10-28 18:06:40 +0100

    encore un commit de totolist

---
+++ temp.js
@@ -0,0 +1,20 @@
+
+/*
+app.get('/sous-sol', function(req, res){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('vous êtes dans ma cave à vin !!');
+});
+
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes dans la chambre à l\'étage n°' +  req.params.etagenum);
+});
+
+app.get('/compteur/:nombre', function(req, res){
+    var noms = ['Emilie', 'Daniella', 'Mamoud', 'un autre crevard'];
+    res.render('compteur.ejs', {compteur:req.params.nombre, noms: noms});
+})
+
+app.get('/middlewares', function(req, res){
+    res.render('middlewares.ejs');
+});*/

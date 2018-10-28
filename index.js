7fa5b7caa33cf44889006c8955f59440d9a5b658
Scalpel <sc4p4l@gmail.com>
2018-10-28 02:21:15 +0100

    ajout ejs

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ app.get('/', function(req, res){
     res.send('vous êtes à l\'accueil...');
 });
 
+app.get('/sous-sol', function(req, res){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('vous êtes dans ma cave à vin !!');
+});
+
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes dans la chambre à l\'étage n°' +  req.params.etagenum);
+});
+
+
 app.listen(8080);
